2ec7310ce402a8883dc43935ef7217194749f8ab
wuhyuk <dngur1331@naver.com>
2025-10-10 16:04:10 +0900

    week04 final

--- AppWeek04/app/src/main/java/com/appweek04/CounterActivity.kt
+++ AppWeek04/app/src/main/java/com/appweek04/CounterActivity.kt
@@ -2,7 +2,7 @@ package com.appweek04
 
 import android.os.Bundle
 import android.util.Log
-import android.view.View
+import android.widget.TextView
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,25 +13,25 @@ class CounterActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_counter)
 
-        val textViewCount = findViewById<textView>(R.id.textViewCount)
-        val buttonMinus = findViewById<Btton>(R.id.buttonMinus)
-        val buttonPlus = findViewById<Btton>(R.id.buttonPlus)
-        val buttonReset = findViewById<Btton>(R.id.buttonReset)
+        val textViewCount = findViewById<TextView>(R.id.textViewCount)
+        val buttonMinus = findViewById<Button>(R.id.buttonMinus)
+        val buttonPlus = findViewById<Button>(R.id.buttonPlus)
+        val buttonReset = findViewById<Button>(R.id.buttonReset)
 
         buttonMinus.setOnClickListener {
             count--
-            textViewCount.text = count.toSting()
-            log.d("kotlinWeek04app", "${count}")
+            textViewCount.text = count.toString()
+            Log.d("kotlinWeek04app", "${count}")
         }
         buttonPlus.setOnClickListener {
             count++
-            textViewCount.text = count.toSting()
-            log.d("kotlinWeek04app", "${count}")
+            textViewCount.text = count.toString()
+            Log.d("kotlinWeek04app", "${count}")
         }
         buttonReset.setOnClickListener {
             count = 0
-            textViewCount.text = count.toSting()
-            log.d("kotlinWeek04app", "${count}")
+            textViewCount.text = count.toString()
+            Log.d("kotlinWeek04app", "${count}")
         }
     }
 }
